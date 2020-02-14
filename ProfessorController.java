b5c315c751c53af19332b9dcd78989c342003517
julia-mak00 <45073758+julia-mak00@users.noreply.github.com>
2020-02-14 11:28:07 -0600

    Update ProfessorController.java

--- ProfessorController.java
+++ ProfessorController.java
@@ -1,9 +1,8 @@
 package com.example.demo;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import java.util.Optional;
 
 @RestController
 public class ProfessorController {
@@ -11,9 +10,29 @@ public class ProfessorController {
     @Autowired
     private ProfessorRepository professorRepository;
 
-    @GetMapping("/professor_info")
+    @GetMapping("/professors")
     public @ResponseBody Iterable<Professor_Info> getAllProfessors() {
         return professorRepository.findAll();
     }
-    
+
+    @GetMapping("/professors/{id}")
+    public @ResponseBody
+    Optional<Professor_Info> getProfessor(@PathVariable String id) {
+        return professorRepository.findById(id);
+    }
+
+    @PostMapping("/professors")
+    public @ResponseBody void createProfessor(@RequestBody Professor_Info professor) {
+        professorRepository.save(professor);
+    }
+
+    @PutMapping("/professors/{id}")
+    public @ResponseBody void updateProfessor(@RequestBody Professor_Info professor) {
+        professorRepository.save(professor);
+    }
+
+    @DeleteMapping("/professors/{id}")
+    public @ResponseBody void deleteProfessor(@PathVariable String id) {
+        professorRepository.deleteById(id);
+    }
 }
