0ad8c85c8a0ff0a1c518b5e344e7a91d08dd2633
julia-mak00 <45073758+julia-mak00@users.noreply.github.com>
2020-02-14 20:52:26 -0600

    Update CourseController.java

--- CourseController.java
+++ CourseController.java
@@ -4,6 +4,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.Optional;
 
+@CrossOrigin(origins = "http://localhost:3000")
 @RestController
 public class CourseController {
     @Autowired
