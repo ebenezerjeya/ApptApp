f649679206bea1c1759c985fc0db98b979d217cd
Ebenezer Jeya <ebenezer.jeya@gmail.com>
2020-03-13 20:29:25 -0500

    Add files via upload

---
+++ src/main/java/com/project/appt/Services/AvailableService.java
@@ -0,0 +1,20 @@
+package com.project.appt.Services;
+
+import com.project.appt.RepoInterfaces.AvailableTimesRepoInterface;
+import com.project.appt.Repositories.AvailableTimesRepository;
+import com.project.appt.Tables.Available_times;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.sql.Date;
+import java.util.List;
+
+@Service
+public class AvailableService implements AvailableTimesRepoInterface {
+
+    @Autowired
+    private  AvailableTimesRepository availableTimesRepository;
+
+    @Override
+    public List<Available_times> findavailable_timesByDate(String id, Date date) {return availableTimesRepository.findavailable_timesByDate(id, date);}
+}
