ad6e7ff5341c05f7af77bddb635e2b1fb071c249
Ebenezer Jeya <ebenezer.jeya@gmail.com>
2020-04-09 18:34:48 -0500

    Add files via upload

---
+++ src/main/java/com/project/appt/Tables/Available_times.java
@@ -0,0 +1,98 @@
+package com.project.appt.Tables;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.criteria.CriteriaBuilder;
+import java.sql.Time;
+import java.util.Date;
+
+@Entity
+public class Available_times {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer available_ID;
+    private String location;
+    private String day;
+    private Time start_time;
+    private Time end_time;
+    private java.sql.Date date;
+    private Boolean available;
+    private String professor_ID;
+
+    public Available_times(){
+    }
+
+    public Available_times(Integer available_ID, String location, String day, Time start_time, Time end_time, java.sql.Date date, Boolean available, String professor_ID) {
+        this.available_ID = available_ID;
+        this.available = available;
+        this.date = date;
+        this.start_time = start_time;
+        this.end_time = end_time;
+        this.location = location;
+        this.day = day;
+        this.professor_ID = professor_ID;
+    }
+    public  Integer getAvailable_ID(){
+        return available_ID;
+    }
+    public void setAvailable_ID(Integer available_ID){
+        this.available_ID = available_ID;
+    }
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+    public String getDay() {
+        return day;
+    }
+
+    public void setDay(String day) {
+        this.day = day;
+    }
+
+    public Date getStart_time() {
+        return start_time;
+    }
+
+    public void setStart_time(Time start_time) {
+        this.start_time = start_time;
+    }
+
+    public Date getEnd_time() {
+        return end_time;
+    }
+
+    public void setEnd_time(Time end_time) {
+        this.end_time = end_time;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(java.sql.Date date) {
+        this.date = date;
+    }
+
+    public Boolean getAvailable() {
+        return available;
+    }
+
+    public void setAvailable(Boolean available) {
+        this.available = available;
+    }
+
+    public String getProfessor_ID() {
+        return professor_ID;
+    }
+
+    public void setProfessor_ID(String professor_ID) {
+        this.professor_ID = professor_ID;
+    }
+
+}
