5770e2ce334fe70cd66ead2ecbdbdca81da79402
Ebenezer Jeya <ebenezer.jeya@gmail.com>
2020-04-09 18:33:54 -0500

    Add files via upload

--- src/main/java/com/project/appt/Repositories/AvailableTimesRepository.java
+++ src/main/java/com/project/appt/Repositories/AvailableTimesRepository.java
@@ -1,9 +1,17 @@
 package com.project.appt.Repositories;
 
-import com.project.appt.Tables.available_times;
+import com.project.appt.Tables.Available_times;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
-public interface AvailableTimesRepository extends CrudRepository<available_times, Integer> {
+import java.sql.Date;
+import java.util.List;
+
+@Repository
+public interface AvailableTimesRepository extends CrudRepository<Available_times, Integer> {
+    @Query("select a from Available_times a where professor_id = ?1 and date = ?2 and available = 1")
+    List<Available_times> findavailable_timesByDate(String id, Date date);
 
 }
 
