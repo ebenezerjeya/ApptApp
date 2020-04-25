ab277109fa4a86dbe466edb4eeadfcd95536a31f
Ka Wai Ng <43895600+kawai-ng@users.noreply.github.com>
2020-04-25 16:53:54 -0500

    Add files via upload

--- src/main/java/com/project/appt/Controllers/AppointmentController.java
+++ src/main/java/com/project/appt/Controllers/AppointmentController.java
@@ -4,7 +4,7 @@ import com.project.appt.RepoInterfaces.ProfessorRepoInterface;
 import com.project.appt.RepoInterfaces.StudentRepoInterface;
 import com.project.appt.Repositories.AvailableTimesRepository;
 import com.project.appt.Repositories.ProfessorRepository;
-import com.project.appt.Repositories.StudentRepository;
+import com.project.appt.Services.EmailServiceImpl;
 import com.project.appt.Tables.Appointment;
 import com.project.appt.Repositories.AppointmentRepository;
 import com.project.appt.Tables.Professor_Info;
@@ -12,7 +12,7 @@ import com.project.appt.Tables.available_times;
 import com.project.appt.Tables.student_info;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
-
+import javax.mail.MessagingException;
 import java.util.List;
 import java.util.Optional;
 
@@ -37,65 +37,81 @@ public class AppointmentController {
     @Autowired
     private ProfessorRepoInterface professorRepoInterface;
 
-    @GetMapping("/appointment") //return all appointments
+    @GetMapping("/appointment")
     public @ResponseBody Iterable<Appointment> getAllAppointment() {
         return appointmentRepository.findAll();
     }
 
-    @GetMapping("/appointment/{id}") //return appointment based on student id or professor id
+    @GetMapping("/appointment/{id}")
     public @ResponseBody
     List<Appointment> getAppointment(@PathVariable String id) {
-        List<student_info> student = studentService.findStudentById(id); //find student by the student id
+        List<student_info> student = studentService.findStudentById(id);
         final List<Appointment>[] appointments = new List[]{null};
-
 
         if(!student.isEmpty()) { //if the id is a student ID
             String email = student.get(0).getStudent_email();
-            appointments[0] = appointmentRepository.findAppointmentByStudentEmail(email); //retrieve the appointment by student email
-
-        }else{//if the id is a professor ID
-            Optional<Professor_Info> professors = professorRepository.findById(id); //find professor by the professor id
-            professors.ifPresent(prof -> { //if it's a valid professor id
+            appointments[0] = appointmentRepository.findAppointmentByStudentEmail(email);
+        }
+        else{//if the id is a professor ID
+            Optional<Professor_Info> professors = professorRepository.findById(id);
+            professors.ifPresent(prof -> {
                 String profEmail = prof.getProfessor_email();
-                appointments[0] = appointmentRepository.findAppointmentByProfessorEmail(profEmail); //retrieve the appointment by professor email
+                appointments[0] = appointmentRepository.findAppointmentByProfessorEmail(profEmail);
             });
-
         }
         return appointments[0];
     }
 
-    @PostMapping("/appointment") //create appointment
-    public @ResponseBody void createAppointment(@RequestBody Appointment appointment) {
-        appointment.setAppointment_date(new java.sql.Date((appointment.getAppointment_date().getTime()+24*60*60*1000))); //set the appointment date
-        appointmentRepository.save(appointment); //save the appointment object in the database
+    @PostMapping("/appointment")
+    public @ResponseBody void createAppointment(@RequestBody Appointment appointment) throws MessagingException {
+        appointment.setAppointment_date(new java.sql.Date((appointment.getAppointment_date().getTime()+24*60*60*1000)));
+        appointmentRepository.save(appointment);
+        appointmentRepository.findById(appointment.getAppointment_id());
 
         // change available times
         List<Professor_Info> prof = professorRepoInterface.findProfByEmail(appointment.getProfessor_Email());
         String prof_id = prof.get(0).getProfessor_id();
+
         List<available_times> time = availableTimesRepoInterface.findavailable_time(prof_id, appointment.getAppointment_date(), appointment.getStart_time());
         available_times selected = time.get(0);
-        selected.setAvailable(false); //set the available times to false for the specific professor
+        selected.setAvailable(false);
         availableTimesRepository.save(selected);
+
+        EmailServiceImpl e = new EmailServiceImpl();
+        e.sendSimpleMessage(appointment.getProfessor_Email(), appointment.getStudent_Email(),
+                "[StudentAppointment] " + appointment.getPurpose(), "<h1>New appointment scheduled on " +
+                        appointment.getAppointment_date() + "</h1> <h2>Appointment Time: " + appointment.getStart_time() + "</h2>" +
+                        "<h2> Location: " + appointment.getOffice() + "</h2> <br> <p><i>Additional details: " +
+                        appointment.getAppointment_description() + "</i></p>" + " <br><h4>Appointment set by: " +
+                        appointment.getStudent_Email() + "</h4>");
+
     }
 
-    @PutMapping("/appointment/{id}")
-    public @ResponseBody void updateAppointment(@RequestBody Appointment appointment) {
-        appointmentRepository.save(appointment);
-    }
-
-    @DeleteMapping("/appointment/{id}") //delete appointment
+    @DeleteMapping("/appointment/{id}")
     public @ResponseBody void deleteAppointment(@PathVariable int id) {
-        Optional<Appointment> appt = appointmentRepository.findById(id); //find the appointment by the appointment id
+        Optional<Appointment> appt = appointmentRepository.findById(id);
 
-        appt.ifPresent(appointment -> { //if the appointment id is valid
+        appt.ifPresent(appointment -> {
 
             // change available times
             List<Professor_Info> prof = professorRepoInterface.findProfByEmail(appointment.getProfessor_Email());
             String prof_id = prof.get(0).getProfessor_id();
+
             List<available_times> time = availableTimesRepoInterface.findavailable_time(prof_id, appointment.getAppointment_date(), appointment.getStart_time());
             available_times selected = time.get(0);
-            selected.setAvailable(true); //set the available times to true for the specific professor
-            appointmentRepository.deleteById(id); //delete the appointment
+            selected.setAvailable(true);
+            appointmentRepository.deleteById(id);
+
+            EmailServiceImpl e = new EmailServiceImpl();
+            try {
+                e.sendSimpleMessage(appointment.getProfessor_Email(), appointment.getStudent_Email(),
+                        "[StudentAppointment Cancellation]", "<h1>Appointment cancelled on " +
+                                appointment.getAppointment_date() + "</h1> <h2>Time slot freed up: " + appointment.getStart_time() + "</h2>" +
+                                "<h2> Location: " + appointment.getOffice() + "</h2> <br> " + " <br><h4>Appointment cancelled by: " +
+                                appointment.getStudent_Email() + "</h4>");
+            } catch (MessagingException messagingException) {
+                messagingException.printStackTrace();
+            }
         });
     }
 }
--- src/main/java/com/project/appt/Controllers/Available_timesController.java
+++ src/main/java/com/project/appt/Controllers/Available_timesController.java
@@ -1,6 +1,8 @@
 package com.project.appt.Controllers;
 import com.project.appt.RepoInterfaces.AvailableTimesRepoInterface;
 import com.project.appt.Repositories.AvailableTimesRepository;
+import com.project.appt.Repositories.ProfessorRepository;
+import com.project.appt.Tables.Professor_Info;
 import com.project.appt.Tables.available_times;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
@@ -19,16 +21,13 @@ public class Available_timesController {
     @Autowired
     private AvailableTimesRepoInterface availableService;
 
+    @Autowired
+    private ProfessorRepository professorRepository;
+
     @GetMapping("/available_times")
     public @ResponseBody Iterable<available_times> getAllTimes() {
         return availableTimesRepository.findAll();
     }
-
-//    @GetMapping("/available_times/{available_ID}")
-//    public @ResponseBody
-//    Optional<available_times> getTime(@PathVariable Integer available_ID) {
-//        return availableTimesRepository.findById(available_ID);
-//    }
 
     @GetMapping("/available_times/{id}")
     public @ResponseBody
@@ -36,9 +35,35 @@ public class Available_timesController {
         return availableService.findavailable_timeByProfessorID(id);
     }
 
-
     @PostMapping("/available_times")
     public @ResponseBody void createTime(@RequestBody available_times available_ID) {
+        // re-adjust the date and set available variable
+        available_ID.setDate(new java.sql.Date((available_ID.getDate().getTime()+24*60*60*1000)));
+        available_ID.setAvailable(true);
+
+        // find professor to set Location
+        Optional<Professor_Info> prof = professorRepository.findById(available_ID.getProfessor_ID());
+        prof.ifPresent(professor_info -> {
+            if(professor_info.getMic_office() != null)
+                available_ID.setLocation(professor_info.getMic_office());
+            if(professor_info.getWarr_office() != null)
+                available_ID.setLocation(professor_info.getWarr_office());
+        });
+
+        // get the day based on the date
+        java.util.GregorianCalendar cal = new java.util.GregorianCalendar();
+        cal.setTime(available_ID.getDate());
+        int day = cal.get(java.util.Calendar.DAY_OF_WEEK);
+        switch(day) {
+            case 0: available_ID.setDay("Sunday"); break;
+            case 1: available_ID.setDay("Monday"); break;
+            case 2: available_ID.setDay("Tuesday"); break;
+            case 3: available_ID.setDay("Wednesday"); break;
+            case 4: available_ID.setDay("Thursday"); break;
+            case 5: available_ID.setDay("Friday"); break;
+            case 6: available_ID.setDay("Saturday"); break;
+        }
+
         availableTimesRepository.save(available_ID);
     }
 
@@ -56,7 +81,6 @@ public class Available_timesController {
     public  @ResponseBody Iterable<available_times> getAvailableTimesByDate(@PathVariable String id, @PathVariable String date){
 
         Date convertedDate = Date.valueOf(date);
-        //List<available_times> Available_times = availableService.findavailable_timesByDate(id, convertedDate);
         return availableService.findavailable_timesByDate(id, convertedDate);
     }
 }
